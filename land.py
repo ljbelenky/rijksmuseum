114f087a30d098caaa9e6a5d71cfd7f2f87ea2b4
ljbelenky <ljbelenky@gmail.com>
2018-01-08 21:23:15 -0700

    with intermittent save

--- land.py
+++ land.py
@@ -1,8 +1,6 @@
 # https://rijksmuseum.github.io/
 
 from api_key import key
-# from bs4 import BeautifulSoup
-# import urllib3
 import requests
 import pandas as pd
 
@@ -13,26 +11,33 @@ import pandas as pd
 # r1 = 'https://www.rijksmuseum.nl/api/en/collection?key=fakekey&format=json'.replace('fakekey', key) #this one returns count = 620338
 # r1 = 'https://www.rijksmuseum.nl/api/en/collection?key=fakekey&format=json&ps=100&p=20'.replace('fakekey', key)  #returns 340479
 
-r1 = 'https://www.rijksmuseum.nl/api/en/collection?key=fakekey&format=json&ps=100&p=page'.replace('fakekey', key)  #returns 340479
-
+r1 = 'https://www.rijksmuseum.nl/api/en/collection?key={}&format=json&ps=100&p={}'
 
 count = 620338
 page = 0
-result = []
-while count > 100:
+
+while count > 0:
+    result = []
     count -=100
     page +=1
     print('Requesting page ',page)
-    r1 = r1.replace('page',str(page))
-    response = requests.get(r1)
+    r2 = r1.format(key,page)
+    print(r2)
+    response = requests.get(r2)
     response_dictionary = response.json()
     art_objects = response_dictionary['artObjects']
+    columns = list(art_objects[0].keys())
+
     for i in art_objects:
         result.append(list(i.values()))
-
-
-df = pd.DataFrame(result)
-df.to_csv('rijksmuseum.csv', encoding = 'utf-8')
+    df = pd.DataFrame(result, columns = columns)
+    df.to_csv('rijksmuseum000{}.csv'.format(page), encoding = 'utf-8')
+#
+#
+#
+#
+# df = pd.DataFrame(result)
+# df.to_csv('rijksmuseum.csv', encoding = 'utf-8')
 
 
 
